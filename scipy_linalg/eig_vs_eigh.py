911577aed6644ffb158d56e472be534e46b5170b
AlexiaNomena <missralexia1@gmail.com>
2022-10-28 23:20:15 +0200

    small_changes

--- scipy_linalg/eig_vs_eigh.py
+++ scipy_linalg/eig_vs_eigh.py
@@ -13,24 +13,23 @@ print("M is symmetric:", np.all(np.isclose(M, M.T))) ### test symmetry of M
 M[np.isclose(M, np.zeros(M.shape))] = 0 # remove numerical zeros
 
 E, U = splinalg.eigh(M) ### eigendecomposition for symmetric matrices
-E = np.real(E) # remove tiny imaginary numbers
-U = np.real(U)
-sort = None
-#sort = np.argsort(E)[::-1] # descending order
-#E = E[sort]
-#U = U[:, sort]
+sort = False
+
 
 Ec, Uc = splinalg.eig(M) ### eigendecomposition for general matrices
-Ec = np.real(Ec) # remove tiny imaginary numbers
-Uc = np.real(Uc) 
-sortc = None   
-#sortc = np.argsort(Ec)[::-1] # descending order
-#Ec = Ec[sortc]
-#Uc = Uc[:, sortc]
+sortc = False   
 
-if sort is not None:
+if sort:
+	E = np.real(E) # remove tiny imaginary parts (because in fact M is positive semi-definite)
+	sort = np.argsort(E)[::-1] # descending order
+	E = E[sort]
+	U = U[:, sort]
 	print("All eigenvalues are equal:", np.all(np.isclose(E, Ec))) # True
-if sortc is not None:
+if sortc:
+	Ec = np.real(E) # remove tiny imaginary parts (because in fact M is positive semi-definite)
+	sortc = np.argsort(Ec)[::-1] # descending order
+	Ec = Ec[sortc]
+	Uc = Uc[:, sortc]
 	print("All eigenvectors are equal:", np.all(np.isclose(U, Uc))) # False
 
 print("eigh eigenvalues", E)
@@ -43,7 +42,6 @@ Gram = tX0.dot(tX0.T)
 SSc = np.sqrt(np.diag(Ec)) 
 tX0c = np.real(Uc.dot(SSc))
 Gramc = tX0c.dot(tX0c.T)
-
 
 print("Gram matrices are the same:", np.all(np.isclose(Gram,Gramc))) # False
 print("sp.linalg.eigh recovers M:", np.all(np.isclose(Gram, M))) # True
